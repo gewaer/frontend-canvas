e8da0073099f8844ae7b33aa23fb69242bd5a45d
Dawin Camilo Cortes <dawin@mctekk.com>
2018-12-14 12:13:47 -0400

    Add webhooks settings

--- src/views/settings/apps/index.vue
+++ src/views/settings/apps/index.vue
@@ -1,19 +1,20 @@
 <template>
-<div class="apps-settings">
-    <h4 class="section-title p-l-10">Settings</h4>
-    <div class="card card-transparent">
-        <div class="card-body no-padding">
-            <div class="row">
-                <div class="col">
-                    <div class="card flex-md-row">
-                        <ul id="tab-3" class="nav nav-tabs nav-tabs-simple nav-tabs-left bg-white">
-                            <li class="nav-item">
-                                <a :class="{active: tab == 'webhooks'}" href="#" @click="tab = 'webhooks'">Webhooks</a>
-                            </li>
-                        </ul>
-                        <div class="tab-content bg-white">
-                            <div id="user-profile" :class="{active: tab == 'webhooks'}" class="tab-pane">
-                                <app-webhooks />
+    <div class="apps-settings">
+        <h4 class="section-title p-l-10">Settings</h4>
+        <div class="card card-transparent">
+            <div class="card-body no-padding">
+                <div class="row">
+                    <div class="col">
+                        <div class="card flex-md-row">
+                            <ul id="tab-3" class="nav nav-tabs nav-tabs-simple nav-tabs-left bg-white">
+                                <li class="nav-item">
+                                    <a :class="{active: tab == 'webhooks'}" href="#" @click="tab = 'webhooks'">Webhooks</a>
+                                </li>
+                            </ul>
+                            <div class="tab-content bg-white">
+                                <div id="user-profile" :class="{active: tab == 'webhooks'}" class="tab-pane">
+                                    <app-webhooks />
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -21,15 +22,14 @@
             </div>
         </div>
     </div>
-</div>
 </template>
 
 <script>
-import appWebhooks from './webhooks.vue';
+import appWebhooks from "./webhooks/";
 
 export default {
     components: {
-        appWebhooks,
+        appWebhooks
     },
     data() {
         return {
---
+++ src/views/settings/apps/webhooks/list.vue
@@ -0,0 +1,86 @@
+<template>
+<div>
+    <h5>
+        Webhooks
+        <button class="btn btn-primary" @click="webhooksCRUD">Add webhook</button>
+    </h5>
+    <div class="table-responsive">
+        <table class="table table-hover table-condensed">
+            <thead>
+                <tr>
+                    <th style="width:50%">Event</th>
+                    <th style="width:20%">Format</th>
+                    <th style="width:20%">URL</th>
+                    <th style="width:10%">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="v-align-middle semi-bold">Cart creation</td>
+                    <td class="v-align-middle semi-bold">JSON</td>
+                    <td class="v-align-middle semi-bold">www.google.com</td>
+                    <td class="v-align-middle semi-bold">
+                        <button class="btn btn-complete m-l-5"><i class="fa fa-edit" aria-hidden="true"/></button>
+                        <button class="btn btn-danger m-l-5"><i class="fa fa-trash" aria-hidden="true" /></button>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="v-align-middle semi-bold">Cart creation</td>
+                    <td class="v-align-middle semi-bold">JSON</td>
+                    <td class="v-align-middle semi-bold">www.google.com</td>
+                    <td class="v-align-middle semi-bold">
+                        <button class="btn btn-complete m-l-5"><i class="fa fa-edit" aria-hidden="true"/></button>
+                        <button class="btn btn-danger m-l-5"><i class="fa fa-trash" aria-hidden="true" /></button>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="v-align-middle semi-bold">Cart creation</td>
+                    <td class="v-align-middle semi-bold">JSON</td>
+                    <td class="v-align-middle semi-bold">www.google.com</td>
+                    <td class="v-align-middle semi-bold">
+                        <button class="btn btn-complete m-l-5"><i class="fa fa-edit" aria-hidden="true"/></button>
+                        <button class="btn btn-danger m-l-5"><i class="fa fa-trash" aria-hidden="true" /></button>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="v-align-middle semi-bold">Cart creation</td>
+                    <td class="v-align-middle semi-bold">JSON</td>
+                    <td class="v-align-middle semi-bold">www.google.com</td>
+                    <td class="v-align-middle semi-bold">
+                        <button class="btn btn-complete m-l-5"><i class="fa fa-edit" aria-hidden="true"/></button>
+                        <button class="btn btn-danger m-l-5"><i class="fa fa-trash" aria-hidden="true" /></button>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="v-align-middle semi-bold">Cart creation</td>
+                    <td class="v-align-middle semi-bold">JSON</td>
+                    <td class="v-align-middle semi-bold">www.google.com</td>
+                    <td class="v-align-middle semi-bold">
+                        <button class="btn btn-complete m-l-5"><i class="fa fa-edit" aria-hidden="true"/></button>
+                        <button class="btn btn-danger m-l-5"><i class="fa fa-trash" aria-hidden="true" /></button>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="v-align-middle semi-bold">Cart creation</td>
+                    <td class="v-align-middle semi-bold">JSON</td>
+                    <td class="v-align-middle semi-bold">www.google.com</td>
+                    <td class="v-align-middle semi-bold">
+                        <button class="btn btn-complete m-l-5"><i class="fa fa-edit" aria-hidden="true"/></button>
+                        <button class="btn btn-danger m-l-5"><i class="fa fa-trash" aria-hidden="true" /></button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    methods: {
+        webhooksCRUD() {
+            this.$emit("webhooksCRUD");
+        }
+    }
+};
+</script>
