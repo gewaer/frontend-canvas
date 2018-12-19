c528b063745ecc8b0b8040519d594093ef5ca4b9
Dawin Camilo Cortes <dawin@mctekk.com>
2018-12-19 17:32:57 -0400

    Wip

---
+++ src/views/settings/apps/custom-fields/crud.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h5>
+            Add custom field
+        </h5>
+        <div class="row">
+            <div class="col">
+                <div class="form-group required">
+                    <label>Field name</label>
+                    <input
+                        class="form-control"
+                        type="text"
+                        name="field-name"
+                        placeholder="Field Name">
+                </div>
+            </div>
+            <div class="col">
+                <label>Category</label>
+                <multiselect
+                    :multiple="true"
+                    :show-labels="false"
+                    :options="['Category 1', 'Category 2', 'Category 3']"
+                />
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/views/settings/apps/custom-fields/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="row custom-fields-settings">
+        <div class="col">
+            <transition name="fade" mode="out-in">
+                <component
+                    :is="currentComponent"
+                    @customFieldsCRUD="customFieldsCRUD"
+                />
+            </transition>
+        </div>
+    </div>
+</template>
+
+<script>
+import customFieldsList from "./list.vue";
+import customFieldsCRUD from "./crud.vue";
+
+export default {
+    components: {
+        customFieldsList,
+        customFieldsCRUD
+    },
+    data() {
+        return {
+            currentComponent: "customFieldsList"
+        };
+    },
+    methods: {
+        customFieldsCRUD() {
+            this.currentComponent = "customFieldsCRUD";
+        }
+    }
+}
+</script>
