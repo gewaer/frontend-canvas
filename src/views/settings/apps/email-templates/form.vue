a4aff95b0e360728c7728c2c838865ed3184581e
sparohawk <sparohawk@gmail.com>
2018-12-27 16:28:54 -0400

    Added lazy loading to apps settings webhooks and email templates

--- src/views/settings/apps/email-templates/form.vue
+++ src/views/settings/apps/email-templates/form.vue
@@ -1,39 +1,46 @@
 <template>
-    <div>
-        <h5>Create email template</h5>
-        <div class="row">
+    <settings-template>
+        <div class="row email-templates-section">
             <div class="col">
-                <div class="form-group  required">
-                    <label>Title</label>
-                    <input
-                        class="form-control"
-                        type="text"
-                        name="url"
-                        placeholder="Title for email template">
+                <h5>Create Email Template</h5>
+                <div class="row">
+                    <div class="col">
+                        <div class="form-group  required">
+                            <label>Title</label>
+                            <input
+                                class="form-control"
+                                type="text"
+                                name="url"
+                                placeholder="Title for email template">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <quill-editor
+                            ref="editor"
+                            v-model="emailTemplate"
+                            class="emailTemplatesEditor"
+                        />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-xl d-flex justify-content-end mt-2">
+                        <router-link :to="{ name: 'settingsAppsEmailTemplatesList' }" class="btn btn-danger m-r-10">Cancel</router-link>
+                        <button class="btn btn-primary" @click="save()">Save</button>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col">
-                <quill-editor
-                    ref="emailTemplatesEditor"
-                    v-model="emailTemplate"
-                    class="emailTemplatesEditor"
-                />
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12 col-xl d-flex justify-content-end mt-2">
-                <button class="btn btn-danger m-r-10" @click="emailTemplatesList()">Cancel</button>
-                <button class="btn btn-primary" @click="save()">Save</button>
-            </div>
-        </div>
-    </div>
+    </settings-template>
 </template>
 
 <script>
 export default {
-    name: "EmailTemplatesForm",
+    name: "Form",
+    components: {
+        SettingsTemplate: () => import("../tab-container")
+    },
     data() {
         return {
             emailTemplate: ""
@@ -41,16 +48,12 @@ export default {
     },
     computed: {
         quillEditor() {
-            return this.$refs.emailTemplatesEditor.quill;
+            return this.$refs.editor.quill;
         }
     },
     methods: {
         save() {
             // Save code and then show the webhooks list
-            this.emailTemplatesList();
-        },
-        emailTemplatesList() {
-            this.$emit("emailTemplatesList");
         }
     }
 }
