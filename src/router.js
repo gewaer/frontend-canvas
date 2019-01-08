b45aa84c313f6504b233dde3ff14a8f1286f1c69
sparohawk <sparohawk@gmail.com>
2019-01-08 14:29:39 -0400

    Fixing typo

--- src/router.js
+++ src/router.js
@@ -39,7 +39,7 @@ const router = new Router({
         },
         {
             path: "/users/forgot-password",
-            name: "forgotPa",
+            name: "forgotPassword",
             component: Auth,
             meta: {
                 requiresAuth: false
