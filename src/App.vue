804ce27ee928d243e1d446536ca102c305a9d58d
SparoHawk <sparohawk@gmail.com>
2019-05-06 15:29:01 -0400

    Removing lang html attribute

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template lang="html">
+<template>
     <div id="app" :class="{ 'full-height' : !($route.meta && $route.meta.requiresAuth == undefined) }">
         <!-- <modals-container/> -->
         <notifications/>
