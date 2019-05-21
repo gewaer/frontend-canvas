b3ba519bedd6d222c9bd638cfc5b570fb8ddd399
SparoHawk <sparohawk@gmail.com>
2019-05-20 20:10:21 -0400

    Extending browse options and functionality

--- src/views/browse/index.vue
+++ src/views/browse/index.vue
@@ -1,14 +1,53 @@
 <template>
-    <gw-browse/>
+    <gw-browse
+        :bulk-actions="bulkActions"
+        :bulk-actions-methods="bulkActionsMethods"
+        :http-options="{ baseURL, headers: { Authorization: token }}"
+        :resources="resources"
+    />
 </template>
 
 <script>
+import { mapState } from "vuex";
 import GwBrowse from "@/npm-components/gw-browse/src/browse"
 
 export default {
     name: "Browse",
     components: {
         GwBrowse
+    },
+    data() {
+        return {
+            baseURL: process.env.VUE_APP_BASE_API_URL,
+            bulkActions: [
+                {
+                    name: "Export",
+                    action: "exportRows"
+                },
+                {
+                    name: "Delete",
+                    action: "deleteRows"
+                }
+            ],
+            bulkActionsMethods: {
+                deleteRows: this.deleteRows,
+                exportRows: this.exportRows
+            },
+            token: this.$store.state.User.token || Cookies.get("token")
+        }
+    },
+    computed: {
+        ...mapState({
+            resources: state => state.Company.data.resources
+        })
+    },
+    methods: {
+        deleteRows() {
+            console.log("rows deleted")
+        },
+        exportRows() {
+            console.log("rows exported")
+        }
     }
 }
 </script>
