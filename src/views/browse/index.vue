11b356967405f40a5ff96bbf33fa6e35766539cd
SparoHawk <sparohawk@gmail.com>
2019-05-22 17:57:55 -0400

    Final tweaking of options for customization

--- src/views/browse/index.vue
+++ src/views/browse/index.vue
@@ -1,6 +1,8 @@
 <template>
     <gw-browse
+        ref="gwBrowse"
         :http-options="{ baseURL, headers: { Authorization: token }}"
+        :pagination-data="paginationData"
         :resources="resources"
     />
 </template>
@@ -24,6 +26,31 @@ export default {
         ...mapState({
             resources: state => state.Company.data.resources
         })
+    },
+    methods: {
+        paginationData(data) {
+            const paginationData = {
+                total: parseInt(data.total_rows),
+                per_page: parseInt(data.limit),
+                current_page: parseInt(data.page),
+                last_page: parseInt(data.total_pages)
+            }
+
+            const nextParams = this.$refs.gwBrowse.getAllQueryParams();
+            nextParams.page = nextParams.page == paginationData.last_page ? null : nextParams.page + 1;
+            const prevParams = this.$refs.gwBrowse.getAllQueryParams();
+            prevParams.page = prevParams.page == 1 ? null : prevParams.page - 1;
+
+            const nextQuery = Object.keys(nextParams).map(key => `${key}=${nextParams[key]}`);
+            const prevQuery = Object.keys(prevParams).map(key => `${key}=${prevParams[key]}`);
+
+            paginationData.next_page_url = nextParams.page === null ? null : `${this.baseURL}?${nextQuery.join("&")}&format=true`;
+            paginationData.prev_page_url = prevParams.page === null ? null : `${this.baseURL}?${prevQuery.join("&")}&format=true`;
+            paginationData.from = (paginationData.current_page - 1) * paginationData.per_page + 1;
+            paginationData.to = paginationData.from + paginationData.per_page - 1;
+
+            return paginationData;
+        }
     }
 }
 </script>
